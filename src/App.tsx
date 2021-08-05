1f62f2c312b73b8341d1d7421c98b911e40ef294
Edward Hong <edward.hong527@gmail.com>
2021-08-05 15:49:09 +1200

    handleAdd functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Button, ButtonGroup } from 'reactstrap'
 import styled from 'styled-components'
 
@@ -5,26 +6,51 @@ import Cart from './Cart'
 import data from './data/items.json'
 import formatNumber from './helper/formatNumber'
 
+interface CartState {
+	name: string
+	price: number
+}
+
 const ListItem = styled.li`
 	margin-bottom: 20px;
 `
 
-const App: React.FC = () => (
-	<div>
-		<Cart value={0} />
-		<ul>
-			{data.map(({ name, price }, i) => (
-				<ListItem key={i}>
-					<p>Item Name: {name}</p>
-					<p>Price: {formatNumber(price)}</p>
-					<ButtonGroup>
-						<Button color="success">Buy</Button>
-						<Button color="danger">Remove</Button>
-					</ButtonGroup>
-				</ListItem>
-			))}
-		</ul>
-	</div>
-)
+const App: React.FC = () => {
+	const [cartItems, setCartItems] = useState<CartState[] | []>([])
+
+	const handleAdd = (name: string, price: number) => () => {
+		if (cartItems.find(item => name === item.name)) {
+			const index = cartItems.findIndex(item => name === item.name)
+			const updatedCartItems = [
+				...cartItems.slice(0, index - 1),
+				{ name: cartItems[index].name, price: cartItems[index].price + price },
+				...cartItems.slice(index + 1),
+			]
+			setCartItems(updatedCartItems)
+		} else {
+			setCartItems([...cartItems, { name, price }])
+		}
+	}
+
+	return (
+		<div>
+			<Cart value={0} />
+			<ul>
+				{data.map(({ name, price }, i) => (
+					<ListItem key={i}>
+						<p>Item Name: {name}</p>
+						<p>Price: {formatNumber(price)}</p>
+						<ButtonGroup>
+							<Button onClick={handleAdd(name, price)} color="success">
+								Add
+							</Button>
+							<Button color="danger">Remove</Button>
+						</ButtonGroup>
+					</ListItem>
+				))}
+			</ul>
+		</div>
+	)
+}
 
 export default App
