7e39ca71afbbb1f7256f0b8fd0f0d3f0d99b46ed
Edward Hong <edward.hong527@gmail.com>
2021-08-05 15:02:19 +1200

    Read data and render list of items

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import Cart from './Cart'
 
+import data from './data/items.json'
+
 const App = () => (
 	<div>
 		<Cart value={0} />
+		<ul>
+			{data.map(({ name, price }, i) => (
+				<li key={i}>
+					<p>Item Name: {name}</p>
+					<p>Price: {price}</p>
+				</li>
+			))}
+		</ul>
 	</div>
 )
 
