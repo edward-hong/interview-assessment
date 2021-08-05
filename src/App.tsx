468c28074a1f3630739fe4d9051631c89e5839ad
Edward Hong <edward.hong527@gmail.com>
2021-08-05 15:12:38 +1200

    Add styled components and margin bottom to list items

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
 import { Button, ButtonGroup } from 'reactstrap'
+import styled from 'styled-components'
 
 import Cart from './Cart'
 import data from './data/items.json'
 import formatNumber from './helper/formatNumber'
+
+const ListItem = styled.li`
+	margin-bottom: 20px;
+`
 
 const App: React.FC = () => (
 	<div>
 		<Cart value={0} />
 		<ul>
 			{data.map(({ name, price }, i) => (
-				<li key={i}>
+				<ListItem key={i}>
 					<p>Item Name: {name}</p>
 					<p>Price: {formatNumber(price)}</p>
 					<ButtonGroup>
 						<Button color="success">Buy</Button>
 						<Button color="danger">Remove</Button>
 					</ButtonGroup>
-				</li>
+				</ListItem>
 			))}
 		</ul>
 	</div>
